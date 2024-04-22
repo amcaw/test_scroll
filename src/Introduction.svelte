83672551625e2f69befeb5e094a9d95f273d2d4f
amcaw <42608053+amcaw@users.noreply.github.com>
2024-04-22 20:20:26 +0200

    Add files via upload

---
+++ src/Introduction.svelte
@@ -0,0 +1,25 @@
+<p><b>Depuis le tournant du millénaire, l'agriculture biologique en Belgique a connu une ascension remarquable, alimentée par une demande croissante des consommateurs, un soutien gouvernemental accru et une prise de conscience croissante de l'importance de la durabilité environnementale et de la santé. </b></p>
+<p>
+En Belgique, le Bio est un secteur qui en séduit plus dun. Ce secteur a démarré dans les années 90. Le gouvernement belge accordait alors une subvention annuelle par hectare pour la production de légumes, encourageant ainsi les agriculteurs à se convertir au bio. La subvention est alors passée de 300 à 930¬ dans ses premières années.
+</p>
+<h2>Une croissance soutenue</h2>
+<p>La croissance du bio a alors connu un léger ralentissement au début des années 2000. Ce léger ralentissement ne sexplique pas tellement il est minime. Dailleurs, le secteur connaît rapidement un regain en 2004.</p>
+<p>À partir de 2005, lagriculture bio connaît un véritable boom. On ne larrête plus, il séduit de plus en plus de producteurs et de consommateurs.</p>
+<p>Depuis 2012, le secteur na fait que croître, bien quil connaisse un léger ralentissement ces dernières années. L'année 2023 est marquée par une baisse de popularité du secteur biologique. Les voyants sont cependant au vert pour lannée 2024.</p>
+<p>
+<h2>Une demande croissante des consommateurs</h2>
+
+	<p> <p> La montée en puissance de la conscience environnementale et de la santé parmi les consommateurs belges a stimulé la demande de produits biologiques. De plus en plus de personnes recherchent des aliments produits de manière biologique, exempts de pesticides et d'engrais chimiques, ce qui a incité les agriculteurs à répondre à cette demande croissante.
+Malgré une croissance annuelle moyenne d'environ 20% au cours de la dernière décennie, l'agriculture biologique reste un secteur relativement petit en Belgique, ne représentant qu'environ 1% de la surface totale cultivée.
+</p>
+<h2>Soutien gouvernemental et réglementation</h2>
+<p> Les autorités belges ont répondu à cette évolution en mettant en place des politiques de soutien visant à encourager la conversion vers l'agriculture biologique. Des subventions, des incitations financières et des programmes d'accompagnement ont été mis en Suvre pour aider les agriculteurs à adopter des pratiques biologiques. Parallèlement, la réglementation entourant l'agriculture biologique a été renforcée pour garantir que les produits biologiques répondent à des normes strictes de qualité et d'intégrité.</p>
+<h2> Diversification des produits biologiques</h2>
+<p>Outre les produits agricoles de base tels que les fruits, les légumes et les céréales, la Belgique a également connu une diversification des produits biologiques disponibles sur le marché. Des produits transformés, des produits laitiers, des viandes biologiques et même des produits de niche sont devenus de plus en plus accessibles aux consommateurs soucieux de leur santé et de l'environnement.</p>
+<h2> 
+Défis persistants
+</h2>
+<p>Malgré les progrès réalisés, l'agriculture biologique en Belgique reste confrontée à des défis persistants. La concurrence avec l'agriculture conventionnelle, la disponibilité limitée de terres biologiques, les coûts de production plus élevés et la nécessité de maintenir la qualité et l'intégrité des produits biologiques sont autant de défis auxquels le secteur doit faire face.</p>
+<p>
+En conclusion, l'agriculture biologique en Belgique a connu une évolution remarquable au cours des deux dernières décennies, avec un soutien accru des consommateurs, du gouvernement et une attention croissante portée à la durabilité et à la santé. Cette tendance positive semble appelée à se poursuivre, avec l'agriculture biologique qui devient de plus en plus un pilier important du paysage agricole belge.
+</p>
